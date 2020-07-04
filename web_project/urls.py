4b94f6519119fa96d9926ea15e4d4e3c869b381e
Samih Abdeljabbar <abdels6375@gmail.com>
2020-07-04 17:17:40 -0500

    more files

--- web_project/urls.py
+++ web_project/urls.py
@@ -19,3 +19,10 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from django.urls import path
+from hello import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+]
